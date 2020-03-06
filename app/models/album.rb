fa55b59a96a426fcfced33a1648ee09ad47964c9
Alexandre Araujo <alelaraujo@gmail.com>
2020-03-06 16:24:57 +0000

    form

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,4 +1,12 @@
 class Album < ApplicationRecord
   belongs_to :user
   has_one_attached :photo
+
+  def photo_url
+    if self.photo.attached?
+      Rails.application.routes.url_helpers.rails_blob_path(self.photo, only_path: true)
+    else
+      nil
+    end
+  end
 end
